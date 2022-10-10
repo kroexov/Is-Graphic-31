1da1a6d2f3ed7dcf9dd4a55ee68deb343ab37352
Sofia Vyatkina <sonia.vyatkina2@yandex.ru>
2022-10-10 20:52:49 +0300

    feat: add basic file read operations

---
+++ Lab1/Lab1/Models/FileHeaderInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lab1.Models;
+
+public class FileHeaderInfo
+{
+    private string _fileFormat;
+    private int _width;  //or rows
+    private int _height;  //or columns
+    private int _maxGreyLevel;
+
+    public FileHeaderInfo(string header)
+    {
+        string[] headerItems = header.Trim().Split();
+        if (headerItems.Length != 4)
+        {
+            throw new Exception("Damaged file: header doesn't match required format");
+        }
+
+        _fileFormat = headerItems[0];
+        _width = Convert.ToInt32(headerItems[1]);
+        _height = Convert.ToInt32(headerItems[2]);
+        _maxGreyLevel = Convert.ToInt32(headerItems[3]);
+    }
+}
--- Lab1/Lab1/Models/PortableAnyMapModel.cs
+++ Lab1/Lab1/Models/PortableAnyMapModel.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.IO;
 
 namespace Lab1.Models;
 
 public class PortableAnyMapModel
 {
-    // вся логика
+    private byte[] _bytes;
+    
+    private String ExtractHeaderInfo()
+    {
+        String header = "";
+        int lineBreakCounter = 0;
+        int i = 0;
+        const int codeOfLineBreakChar = 10;
 
-    public void AfterOpenFileLogic()
+        while (lineBreakCounter != 3)
+        {
+            if (_bytes[i] == codeOfLineBreakChar)
+            {
+                lineBreakCounter++;
+                header += " ";
+            }
+            else
+            {
+                header += Convert.ToChar(_bytes[i]);
+            }
+
+            i++;
+        }
+
+        return header;
+    }
+
+    public void ReadFile(String filePath)
+    {
+        if (!File.Exists(filePath))
+        {
+            throw new FileNotFoundException();
+        }
+
+        _bytes = File.ReadAllBytes(filePath);
+    }
+
+    public void AfterOpenFileLogic(string filePath)
     {
         //ModelErrorHappened?.Invoke("кринж");
     }
